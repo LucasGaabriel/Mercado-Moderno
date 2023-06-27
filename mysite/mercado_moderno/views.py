a1caf1209331f7dcc972d6db0c7eb2a92831a3b4
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2023-06-27 17:43:25 -0300

    Add page de alteração de senha e autenticação necessária em alguns métodos

--- mysite/mercado_moderno/views.py
+++ mysite/mercado_moderno/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from .models import Produto
+
+def index(request):
+    return render(request, "mercado_moderno/index.html")
 
 def cadastrar_usuario(request):
     if request.method == "POST":
@@ -33,17 +37,28 @@ def deslogar_usuario(request):
     logout(request)
     return redirect('/')
 
+@login_required(login_url='/login')
+def alterar_senha(request):
+    if request.method == "POST":
+        form_senha = PasswordChangeForm(request.user, request.POST)
+        if form_senha.is_valid():
+            user = form_senha.save()
+            update_session_auth_hash(request, user)
+            return redirect('/home')
+    else:
+        form_senha = PasswordChangeForm(request.user)
+    return render(request, 'mercado_moderno/alterar_senha.html', {'form_senha': form_senha})
 
-def index(request):
-    return render(request, "mercado_moderno/index.html")
-
+@login_required(login_url='/login')
 def home(request):
     return render(request, "mercado_moderno/home.html")
 
+@login_required(login_url='/login')
 def produtos(request):
     produtos = Produto.objects.all()
     context = { "produtos": produtos }
     return render(request, "mercado_moderno/produtos.html", context)
 
+@login_required(login_url='/login')
 def carrinho(request):
     return HttpResponse("Você está vendo a página com o seu carrinho, contendo todos os seus produtos desejados!")
