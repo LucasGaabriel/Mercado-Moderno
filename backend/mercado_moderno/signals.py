3ee28d7abbadc2c8da63c35153732a6f939b2d82
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2023-07-09 13:29:23 -0300

    Fix signal para criar carrinho apenas se não existir

--- backend/mercado_moderno/signals.py
+++ backend/mercado_moderno/signals.py
@@ -5,4 +5,4 @@ from .models import Usuario, Carrinho
 @receiver(post_save, sender=Usuario)
 def Criar_Carrinho(sender, instance, created, **kwargs):
     if created:
-        Carrinho.objects.create(usuario=instance)
+        Carrinho.objects.get_or_create(usuario=instance)
