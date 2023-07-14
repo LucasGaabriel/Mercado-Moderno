eec8d9195964ca6135492a43f38b2372a987d0eb
siqueiradaniel <danielsiqueira111@gmail.com>
2023-07-14 16:19:42 -0300

    Adding comments to frontend

--- frontend/src/context/shop-context.jsx
+++ frontend/src/context/shop-context.jsx
@@ -3,7 +3,11 @@ import axios from "axios";
 
 export const ShopContext = createContext(null)
 
-
+/**
+ * Structure that I use to share information between
+ * components, such as the list of products, 
+ * if the user is logged in, the cart elements, etc...
+ */
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({})
     const [products, setProducts] = useState([]);
@@ -11,6 +15,15 @@ export const ShopContextProvider = (props) => {
     const [userId, setUserId] = useState(null);
     
     let cart = {};
+    
+    /**
+     * Fill the products array with the products 
+     * from API.
+     * Fill cartItems with the items in the logged 
+     * user cart or defined cartItems like empty.
+     * Update always when the logged changes or the page
+     * have the hard update (f5)
+     */
     useEffect(() => {
         axios.get("http://127.0.0.1:8080/api/produtos/")
         .then((response) => {
@@ -29,10 +42,21 @@ export const ShopContextProvider = (props) => {
             setCartItems(cart);
         }).catch((error) => console.log(error));
     }, [logged]);
-    
+
+    /**
+     * Find the product id from the products array
+     * @param {Number} id 
+     * @returns {object} product.id == id
+     */
     const findProduct = (id) => {
         return products.find((p) => p.id === Number(id))
     }
+
+    /**
+     * Add one unit of product with same id
+     * as itemId to the API and to the cartItems
+     * @param {Number} itemId 
+     */
     const addToCart = (itemId) => {
         cartItems[itemId] < findProduct(itemId)?.estoque ?
             setCartItems((prev) => {
@@ -46,6 +70,11 @@ export const ShopContextProvider = (props) => {
         : 
             alert(`Temos apenas ${cartItems[itemId]} unidades de ${findProduct(itemId)?.nome}!`);
     }
+    /**
+     * Remove one unit of product with same id
+     * as itemId to the API and to the cartItems
+     * @param {Number} itemId 
+     */
     const removeFromCart = (itemId) => {
         if(cartItems[itemId] > 0) {
             setCartItems((prev) => {
@@ -60,6 +89,12 @@ export const ShopContextProvider = (props) => {
             alert(`${findProduct(itemId)?.nome} removido(a) do carrinho`);
     }
 
+    /**
+     * Changes element value in cartItems and 
+     * API which has same id as itemId to newAmount.
+     * @param {Number} newAmount 
+     * @param {Number} itemId 
+     */
     const updateCartItemCount = (newAmount, itemId) => {
         let amount = Number(newAmount);
         let estoque = findProduct(itemId)?.estoque;
@@ -78,6 +113,15 @@ export const ShopContextProvider = (props) => {
         })
     };
     
+    /**
+     * Checkout: 
+     * - Set cartItems and cart of user on api to empty
+     * - Save the buy on API
+     * - Change the sells and stock product value in the API
+     *  and on the "products" array
+     * - Run only if user is logged otherwise alert them
+     * 
+     */
     const checkout = async () => {
         if(logged) {
             axios.post(`http://127.0.0.1:8080/api/compras/${userId}/save`)
@@ -99,6 +143,11 @@ export const ShopContextProvider = (props) => {
         }
     }
 
+    /**
+     * Calculate the total Amount using the 
+     * products array values
+     * @returns {Number} totalAmount
+     */
     const getTotalCartAmount = () => {
         let totalAmount = 0;
 
@@ -112,6 +161,10 @@ export const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    /**
+     * variables I want other parts
+     * of the code to be able to access.
+     */
     const contextValue = {cartItems, products, logged, setLogged, setUserId, addToCart, removeFromCart, checkout, updateCartItemCount, getTotalCartAmount}
     
     return <ShopContext.Provider value={contextValue}>
